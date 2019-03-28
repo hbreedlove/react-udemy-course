224298cedba3ef49048bfa1054788eeeaa5c1ccb
Hayden Breedlove <hbreedlove1@gmail.com>
2019-03-28 15:06:16 -0400

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 //this is a route handler
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 //Dynamic port binding. Use boolean statement to say if Heroku has already defined and evironment variable (deployed), use that, otherwise (development) use port 5000

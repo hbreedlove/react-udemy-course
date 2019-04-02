00e12b7986d40c4ad6eaf88e028ba94517efbeec
Hayden Breedlove <hbreedlove1@gmail.com>
2019-04-02 14:30:12 -0400

    finished auth flow

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-//this is a route handler
-app.get('/', (req, res) => {
-  res.send({ bye: 'buddy' });
-});
+app.use(
+  cookieSession({
+    //needs to be in milliseconds. We chose a length of 30 days per session
+    maxAge: 30 * 24 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//the first part of this statement is returning a function, which immediately uses the second part as an argument
+require('./routes/authRoutes')(app);
 
 //Dynamic port binding. Use boolean statement to say if Heroku has already defined and evironment variable (deployed), use that, otherwise (development) use port 5000
 const PORT = process.env.PORT || 5000;
